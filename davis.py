60cfcb5c46d08faec754eba8685b2287a73d657e
xtcuser <onureser12@gmail.com>
2019-08-26 16:32:00 +0300

    v0.2b

--- davis.py
+++ davis.py
@@ -5,10 +5,15 @@ import os
 import sys
 import time
 import wikipedia
+import configparser
 import selenium
+import random
 from selenium import webdriver
 #from gtts import gTTS
- 
+
+datacounter = 0
+lastdata = ""
+
 def speak(audioString):
     print("davis: " + audioString)
     #tts = gTTS(text=audioString, lang='tr')
@@ -18,9 +23,40 @@ def speak(audioString):
 
 def listen():
     #data = recordAudio()
+    global lastdata
+    global datacounter
     data = input("closx: ")
-    datalw = data.lower()
-    general(datalw)
+    if lastdata == data.lower():
+        datacounter = datacounter + 1
+    else:
+        datacounter = 0
+    if datacounter > 1:
+        speak("Sanırım bunu daha önce konuşmuştuk. Başka ne konuda yardımcı olabilirim?")
+        listen()
+
+    else:
+        datalw = data.lower()
+        lastdata = datalw
+        general(datalw)
+
+def listendev():
+    #data = recordAudio()
+    global lastdata
+    global datacounter
+    data = input("closx: ")
+    if lastdata == data.lower():
+        datacounter = datacounter + 1
+    else:
+        datacounter = 0
+    if datacounter > 1:
+        speak("Sanırım bunu daha önce konuşmuştuk. Başka ne konuda yardımcı olabilirim?")
+        listen()
+
+    else:
+        datalw = data.lower()
+        lastdata = datalw
+        developermode(datalw)
+    
 
 def listensup():
     #data = recordAudio()
@@ -47,10 +83,11 @@ def listenwyd():
     #except sr.UnknownValueError:
         #print("Google Speech Recognition could not understand audio")
     #return data
- 
+
 def general(data):
-    if "merhaba" in data or "selam" in data or "hey" in data:
-        speak("Merhaba, Onur!")
+
+    if data in selam:
+        speak(selamcvp[random.randint(0,2)])
         listen()
 
     if "çıkış" in data:
@@ -59,29 +96,64 @@ def general(data):
         sys.exit()
 
     if "naber" in data:
-        speak("İyiyim Onur, sen nasılsın?")
+        speak(iyisen[random.randint(0,2)])
         listensup()
 
-    if "ne yapıyorsun" in data or "nabıyon" in data or "napıyorsun" in data or "napıyon" in data:
-        speak("Robot olmak dışında hiçbir şey yapmıyorum, Onur. Ya sen?")
+    if data in nabiyon:
+        speak(nabiyoncvp[random.randint(0,2)])
         listenwyd()
 
     if "saat" in data:
         speak("Şuan saat " + time.strftime("%H %M"))
+        listen()
 
     if "teşekkür" in data:
         speak("Rica ederim!")
+        listen()
 
     if "nedir" in data:
         datalw = data.lower()
         wiki(datalw)
 
+    if "geliştirici" in data:
+        speak("Hoşgeldiniz, efendim.")
+        listendev()
+
     else:
-        #speak("Bu komutu bilmiyorum.")
+        speak(anlamadim[random.randint(0,2)])
+        listen()
+
+
+def developermode(data):
+    
+    if "ini" in data and "sil" in data:
+        speak("Nasıl isterseniz. Yeniden başlatma gerekiyor.")
+        os.remove(configpath)
+        os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
+
+    if "genel mod" in data:
+        "Genel moda geçiliyor."
+        listen()
+
+    if "çıkış" in data:
+        speak("Tamam, görüşmek üzere!")
+        #time.sleep(3)
+        sys.exit()
+
+    if "saat" in data:
+        speak("Şuan saat " + time.strftime("%H %M"))
+        listen()
+
+    if "teşekkür" in data:
+        speak("Rica ederim!")
+        listen()
+
+    else:
+        speak(anlamadim[random.randint(0,2)])
         listen()
 
 def process(data):
-    speak("Bu çok güzel!")
+    speak(cokiyi[random.randint(0,3)])
     listen()
 
 def wiki(data):
@@ -97,28 +169,75 @@ def wiki(data):
     listen()
 
 def sup(data):
-    if "ne yapıyorsun" in data or "nabıyon" in data or "napıyorsun" in data or "napıyon" in data:
-        speak("Robot olmak dışında hiçbir şey yapmıyorum, Onur. Ya sen?")
+    if data in nabiyon:
+        speak(nabiyoncvp[random.randint(0,2)])
         listenwyd()
     if "iyi" in data:
         if "değil" in data or "sayılma" in data:
-            speak("Senin için üzüldüm.")
+            speak(cokkotu[random.randint(0,3)])
             listen()
         else:
-            speak("İyi olman çok güzel!")
+            speak(cokiyi[random.randint(0,3)])
             listen()
     if "kötü" in data or "fena" in data:
         if "değil" in data or "sayılma" in data:
-            speak("iyi olman çok güzel!")
+            speak(cokiyi[random.randint(0,3)])
             listen()
         else:
-            speak("Senin için üzüldüm.")
+            speak(cokkotu[random.randint(0,3)])
             listen()
     else:
-        speak("Cevap vermeye niyetin yok sanırım. Senin için başka ne yapabilirim?")
+        speak("Pardon ama bu cevabı anlamadım. Senin için başka ne yapabilirim?")
         listen()
 
 
+configpath = os.path.dirname(os.path.abspath(__file__)) + "/vars.ini"
+config = configparser.ConfigParser()
 
-speak("Merhaba Onur, Sana Nasıl Yardımcı Olabilirim?")
+def dataminer(section):
+    dict1 = {}
+    options = config.options(section)
+    for option in options:
+        try:
+            dict1[option] = config.get(section, option)
+        except:
+            dict1[option] = None
+    return dict1
+
+isim = ""
+
+def frconfig():
+    if(os.path.exists(configpath)):
+        if(os.stat(configpath).st_size == 0):
+            os.remove(configpath)
+
+    if(os.path.exists(configpath)):
+        config.read(configpath)
+        isimlw = config['userdata']['name']
+        global isim 
+        isim = isimlw.capitalize()
+        speak("Merhaba " + isim + ", Sana Nasıl Yardımcı Olabilirim?")
+    else:
+        cfgfile = open(configpath,'w')
+        speak("Merhaba, Ben Davis. Size nasıl hitap etmemi istersiniz?")
+        isiminput = input("closx: ")
+        isim = isiminput.capitalize()
+        isimlw = isim.lower()
+        config.add_section('userdata')
+        config.set('userdata','name',isimlw)
+        config.write(cfgfile)
+        cfgfile.close()
+        speak("Merhaba " + isim + ", Sana Nasıl Yardımcı Olabilirim?")
+
+
+selam = ["selam","hey","merhaba"]
+selamcvp = ["Merhaba!","Merhaba, "+ isim +"!","Seni görmek güzel, "+ isim +"!","Hey, "+ isim +"!"]
+nabiyon = ["ne yapıyorsun","napıyorsun","napıyon","nasıl gidiyo","nasıl gidiyor","nabıyon","napıyosun"]
+nabiyoncvp = ["Robot olmak dışında hiçbir şey yapmıyorum, "+ isim +". Ya sen?","Senin için kendimi geliştirmeye çalışıyorum. Sen?","Öyle takılıyorum. Sen ne yapıyorsun?"]
+cokiyi = ["Bu çok güzel!","Bunu duymak müthiş!","Böyle devam et!","Harikasın!"]
+cokkotu = ["Bu çok kötü!","Bunu duymak berbat!","Umarım böyle devam etmez!","Senin adına üzüldüm!"]
+iyisen = ["İyiyim "+ isim +", sen nasılsın?","İyiyim "+isim+", seni sormalı.","Ben her zamanki gibiyim "+isim+", asıl senden ne haber?"]
+anlamadim = ["Bunun ne demek olduğunu bilmiyorum.","Ne dediğini anlamadım.","Maalesef, ne demek istediğini anlayamadım.","Anlayamadım."]
+
+frconfig()
 listen()
